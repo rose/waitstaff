7333faaf49ff2f267464f48235af7079a337c263
Rose Ames <rose@happyspork.com>
2014-07-21 21:56:34 -0400

    add reset button

--- index.js
+++ index.js
@@ -5,11 +5,21 @@
     $scope.tip = 0;
     $scope.total_meal = 0;
 
+    var myParseFloat = function(str) {
+      var float_regex = /^(0|[1-9]\d*)(\.\d{2})?$/
+      return parseFloat(str.match(float_regex));
+    }
+      
+    $scope.resetTwo = function() {
+      $scope.subtotal = $scope.tip = $scope.total_meal = 0;
+      $scope.base = $scope.tax_percent = $scope.tip_percent = "";
+    }
+
     $scope.pay = function(isValid) {
       if (isValid) {
-        var base = parseFloat($scope.base);
-        var tax_percent = parseInt($scope.tax_percent);
-        var tip_percent = parseInt($scope.tip_percent);
+        var base = myParseFloat($scope.base);
+        var tax_percent = myParseFloat($scope.tax_percent);
+        var tip_percent = myParseFloat($scope.tip_percent);
         if (base === base && tax_percent === tax_percent && tip_percent === tip_percent) {
           $scope.subtotal = base * (1 + tax_percent/100);
           $scope.tip = $scope.subtotal * tip_percent/100;
